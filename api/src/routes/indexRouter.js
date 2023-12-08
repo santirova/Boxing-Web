7409cc22e705ef325a3c2f835ff882e5dcb1c9df
Silrojua22 <117102050+Silrojua22@users.noreply.github.com>
2023-12-08 02:52:24 -0300

    Payments controller, handler and routes

--- api/src/routes/indexRouter.js
+++ api/src/routes/indexRouter.js
@@ -1,20 +1,22 @@
+// indexRouter.js
 const { Router } = require('express');
 const { Payments } = require('../db');
 const { paymentsData } = require('../../api');
+const paymentsRouter = require("./paymentsRouter");
 
 const router = Router();
 
-router.use('/api', async (req, res) => {
+router.use('/payments', paymentsRouter);
 
+router.use('/api', async (req, res) => {
     const allPayments = await Payments.findAll();
 
     if (!allPayments.length) {
         await Payments.bulkCreate(paymentsData);
-
-        res.status(200).json("Abonos Cargados")
+        res.status(200).json("Abonos Cargados");
     } else {
-        res.status(200).json("Abonos ya han sido cargados")
+        res.status(200).json("Abonos ya han sido cargados");
     }
-})
+});
 
 module.exports = router;
