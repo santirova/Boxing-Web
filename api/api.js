3dada21dc01f7a522a414f953c72981f80ffc039
Cristian Monsalvo <cnmonsalvo@gmail.com>
2023-12-10 02:41:19 -0300

    Aplicando CSS

--- api/api.js
+++ api/api.js
@@ -1,19 +1,18 @@
 const paymentsData = [
-    {
-        name: "Por Día",
-        price: 0,
-
-    }, {
-        name: "Por Semana",
-        price: 0,
-
-    },
-    {
-        name: "Por Mes",
-        price: 0,
-    },
+  {
+    name: "Por Día",
+    price: 0,
+  },
+  {
+    name: "Por Semana",
+    price: 0,
+  },
+  {
+    name: "Por Mes",
+    price: 0,
+  },
 ];
 
 module.exports = {
-    paymentsData
+  paymentsData,
 };
