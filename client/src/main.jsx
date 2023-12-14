424dffc99f0302fc6154823e48335f9967859809
Silrojua22 <117102050+Silrojua22@users.noreply.github.com>
2023-12-14 14:52:50 -0300

    Redux.2

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,13 @@
-
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./sass/main.css";
+import { Provider } from "react-redux";
+import store from "../src/redux/store.js";
+import axios from "axios";
 
+axios.defaults.baseURL = "http://localhost:3001";
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
