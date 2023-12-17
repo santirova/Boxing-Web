7111dcd94b7dd94862e207aed1be9c44097ade60
Cristian Monsalvo <cnmonsalvo@gmail.com>
2023-12-16 21:01:06 -0300

    Sass componentes

--- client/src/client-views/price/price.jsx
+++ client/src/client-views/price/price.jsx
@@ -1,27 +1,40 @@
-import ApiCardContainer from "../../client-component/cardContainer/apiCardContainer";
-import "./Price.css";
-import { getPayments } from "../../redux/paymentsActions";
-import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { getPayments } from "../../redux/paymentsActions";
+import ApiCardContainer from "../../client-component/cardContainer/apiCardContainer";
+import "./Price.css";
 
 const Price = () => {
   const dispatch = useDispatch();
   const allPayments = useSelector((state) => state.paymentsReducer.allPayments);
+
   useEffect(() => {
     if (!allPayments.length) {
       axios.get("/api").then(() => dispatch(getPayments()));
     } else {
       dispatch(getPayments());
     }
-  }, [dispatch]);
+  }, [dispatch, allPayments]);
+
   return (
-    <div className="price__container">
-      <h1>Precios</h1>
+    <section className="title__section">
+      <div className="title__content">
+        <h1>Precios</h1>
+        <p>
+          Bienvenido a Boxeo Noriega, donde la pasión por el boxeo se encuentra
+          con el compromiso hacia el rendimiento y la superación personal.
+        </p>
+        <div className="subtitle__section">
+          <h6>
+            Inicio / <span>Precios</span>
+          </h6>
+        </div>
+      </div>
       <div className="card__container">
         <ApiCardContainer />
       </div>
-    </div>
+    </section>
   );
 };
 
---
+++ client/src/client-views/trainning.jsx
@@ -0,0 +1,24 @@
+const Trainning = () => {
+  return (
+    <>
+      <section className="title__section">
+        <div className="title__content">
+          <h1>Entrenamiento</h1>
+          <p>
+            Bienvenido a Boxeo Noriega, donde la pasión por el boxeo se
+            encuentra con el compromiso hacia el rendimiento y la superación
+            personal. Somos una comunidad unida por el amor al deporte y el
+            deseo de alcanzar nuevos límites.
+          </p>
+          <div className="subtitle__section">
+            <h6>
+              Inicio / <span>Entrenamiento</span>
+            </h6>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Trainning;
