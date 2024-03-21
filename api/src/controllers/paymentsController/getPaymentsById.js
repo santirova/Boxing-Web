3d976391627b2926f6bd85c982b6feab7a560b72
Cristian Monsalvo <cnmonsalvo@gmail.com>
2024-03-20 22:06:51 -0300

    Arreglando carpeta API

--- api/src/controllers/paymentsController/getPaymentsById.js
+++ api/src/controllers/paymentsController/getPaymentsById.js
@@ -1,22 +1,13 @@
 const { Payments } = require("../../db");
 
 const getPaymentsById = async (paymentsId) => {
-<<<<<<< HEAD
-    console.log("Esta es la data => ", paymentsId);
-    try {
-        const payments = await Payments.findByPk(paymentsId.toUpperCase());
-        return payments
-    } catch (error) {
-        throw new Error("Error al obtener Abono por ID: " + error.message);
-    }
-=======
+  console.log("Esta es la data => ", paymentsId);
   try {
     const payments = await Payments.findByPk(paymentsId.toUpperCase());
     return payments;
   } catch (error) {
     throw new Error("Error al obtener Abono por ID: " + error.message);
   }
->>>>>>> fd066e3ca39b946f3191dba5742e6243cff51d34
 };
 
 module.exports = {
