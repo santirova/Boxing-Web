e71936d14f83b093d49a4424e9a8895e1e719a59
Silrojua22 <117102050+Silrojua22@users.noreply.github.com>
2024-03-31 04:18:04 -0300

    Session token

--- api/src/routes/auth0Router.js
+++ api/src/routes/auth0Router.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const { getUserHandler } = require("../handlers/userHandlers");
 const { postUserHandler } = require("../handlers/userHandlers");
+const authMiddleware = require("../middleware/session");
 
 const auth0Router = Router();
 
-auth0Router.get("/", getUserHandler);
+auth0Router.get("/", authMiddleware, getUserHandler);
 auth0Router.post("/", postUserHandler);
 module.exports = auth0Router;
