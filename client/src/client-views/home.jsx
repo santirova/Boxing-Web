76f78d3bc0cb0a2c67ac34cad5b4bca00d15f9d9
Cristian Monsalvo <cnmonsalvo@gmail.com>
2024-03-31 13:07:37 -0300

    Home responsive

--- client/src/client-views/home.jsx
+++ client/src/client-views/home.jsx
@@ -1,10 +1,11 @@
-import { Link } from "react-router-dom";
 import img1 from "../assets/box.jpg";
 import img2 from "../assets/box-dos.jpg";
 import img3 from "../assets/box-tres.jpg";
-import { useAuth0 } from "@auth0/auth0-react";
-import { useEffect } from "react";
+import benefits from "../json/benefits.json";
 import axios from "axios";
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const images = [
   {
@@ -37,31 +38,48 @@ const Home = () => {
 
   return (
     <>
-      <div className="home__container">
-        <h1>
-          <span className="s1">Team</span>
-          <span className="s2">
-            Diego <span className="s3">Noriega</span>
-          </span>
-        </h1>
-        <p>Donde cada golpe cuenta una historia</p>
-        <p>de valentía y perseverancia.</p>
-        <Link to="Entrenamiento">
-          <button className="btn-home">Ver más</button>
-        </Link>
-      </div>
-      <div className="subhome__container">
-        <h2>¿Por qué elegirnos?</h2>
-        <div className="linea"></div>
-        <div className="img__container">
-          {images.map((image, index) => (
-            <div key={index} className="img-subhome">
-              <img src={image.src} alt={`Imagen ${index + 1}`} />
-              <p>{image.text}</p>
-            </div>
-          ))}
+      <section className="home__container">
+        <div className="title__container">
+          <h1>
+            <span className="s1">Team</span>
+            <span className="s2">Diego Noriega</span>
+          </h1>
+          <p>Donde cada golpe cuenta una historia</p>
+          <p>de valentía y perseverancia.</p>
+          <Link to="Entrenamiento">
+            <button className="btn-home">Ver más</button>
+          </Link>
         </div>
-      </div>
+      </section>
+      <section className="subhome__container">
+        <div className="second__container">
+          <h2>¿Por qué elegirnos?</h2>
+          <div className="linea"></div>
+          <div className="img__container">
+            {images.map((image, index) => (
+              <div key={index} className="img-subhome">
+                <img src={image.src} alt={`Imagen ${index + 1}`} />
+                <p>{image.text}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+      <section className="benefits__container">
+        <div className="third__title">
+          <h3>Beneficios</h3>
+          <div className="linea"></div>
+          <div className="benefits-container">
+            {benefits.map((benefit, index) => (
+              <div key={index} className="benefit">
+                <img src={benefit.img} alt={benefit.name} />
+                <h4>{benefit.name}</h4>
+                <p>{benefit.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
     </>
   );
 };
