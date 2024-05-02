e9acc561210b738b01ffc12d372533cd27b0f781
Silrojua22 <117102050+Silrojua22@users.noreply.github.com>
2024-05-02 19:23:33 -0300

    Deploy-1

--- api/src/controllers/mercadoPagoController/mercadoPago.js
+++ api/src/controllers/mercadoPagoController/mercadoPago.js
@@ -1,33 +1,33 @@
-// require("dotenv").config();
-// const mercadopago = require("mercadopago");
-// const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+require("dotenv").config();
+const mercadopago = require("mercadopago");
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 
-// mercadopago.configure({
-//   access_token: ACCESS_TOKEN,
-// });
+mercadopago.configure({
+  access_token: ACCESS_TOKEN,
+});
 
-// const mercadoPago = async (pendingOrderId, itemsBody) => {
-//   let preference = {
-//     metadata: { relatedOrderId: pendingOrderId },
-//     items: itemsBody,
+const mercadoPago = async (pendingOrderId, itemsBody) => {
+  let preference = {
+    metadata: { relatedOrderId: pendingOrderId },
+    items: itemsBody,
 
-//     back_urls: {
-//       success: "https://viandaexpress.vercel.app/payment",
-//       failure: "https://viandaexpress.vercel.app/payment",
-//       pending: "https://viandaexpress.vercel.app/payment",
-//     },
-//     auto_return: "approved",
-//   };
+    back_urls: {
+      success: "https://viandaexpress.vercel.app/payment",
+      failure: "https://viandaexpress.vercel.app/payment",
+      pending: "https://viandaexpress.vercel.app/payment",
+    },
+    auto_return: "approved",
+  };
 
-//   try {
-//     const response = await mercadopago.preferences.create(preference);
-//     return {
-//       id: response.body.id,
-//     };
-//   } catch (error) {
-//     console.log(error);
-//     throw new Error("Error creating MercadoPago preference");
-//   }
-// };
+  try {
+    const response = await mercadopago.preferences.create(preference);
+    return {
+      id: response.body.id,
+    };
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error creating MercadoPago preference");
+  }
+};
 
-// module.exports = { mercadoPago };
+module.exports = mercadoPago;
--- api/src/handlers/mercadoPagoHanlders.js
+++ api/src/handlers/mercadoPagoHanlders.js
@@ -1,30 +1,28 @@
-// const {
-//   mercadoPago,
-// } = require("../controllers/mercadoPagoController/mercadopago.js");
-// const {
-//   getPendingOrderByUserEmailController,
-// } = require("../controllers/orderController/getPendingOrderByUserEmailController");
+const mercadoPago = require("../controllers/mercadoPagoController/mercadopago.js");
+const {
+  getPendingOrderByUserEmailController,
+} = require("../controllers/orderController/getPendingOrderByUserEmailController");
 
-// const mercadoPagoHandler = async (req, res) => {
-//   try {
-//     const { userEmail } = req.params;
-//     const itemsBody = req.body.map((item) => {
-//       return {
-//         title: item.Payments.name,
-//         unit_price: item.final_price,
-//         quantity: item.quantity,
-//       };
-//     });
-//     const pendingOrderId = await getPendingOrderByUserEmailController(
-//       userEmail
-//     );
+const mercadoPagoHandler = async (req, res) => {
+  try {
+    const { userEmail } = req.params;
+    const itemsBody = req.body.map((item) => {
+      return {
+        title: item.Payments.name,
+        unit_price: item.final_price,
+        quantity: item.quantity,
+      };
+    });
+    const pendingOrderId = await getPendingOrderByUserEmailController(
+      userEmail
+    );
 
-//     const preferenceId = await mercadoPago(pendingOrderId, itemsBody);
+    const preferenceId = await mercadoPago(pendingOrderId, itemsBody);
 
-//     res.status(200).json(preferenceId);
-//   } catch (error) {
-//     res.status(400).send({ error: error.message });
-//   }
-// };
+    res.status(200).json(preferenceId);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+};
 
-// module.exports = { mercadoPagoHandler };
+module.exports = { mercadoPagoHandler };
